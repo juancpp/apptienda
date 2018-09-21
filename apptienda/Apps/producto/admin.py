ee9f46d4b742b79d86b2f6d838ab1d16967fe6f8
juancpp <juancpp@gmail.com>
2018-09-21 15:54:40 -0300

    Inicializando el proyecto

---
+++ apptienda/Apps/producto/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import accesorio, articulo
+
+# Register your models here.
+admin.site.register(accesorio)
+admin.site.register(articulo)
---
+++ apptienda/Apps/producto/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from Apps.retiro.models import retiro
+
+# Create your models here.
+class accesorio(models.Model):
+	tipo = models.CharField(max_length=50)
+
+class articulo(models.Model):
+	tipo_art = models.CharField(max_length=50)
+	talla = models.CharField(max_length=3)
+	color = models.CharField(max_length=10)
+	fecha_llegada = models.DateField()
+	cantidad = models.IntegerField()
+	retiro = models.ForeignKey(retiro, null=True, blank=True, on_delete=models.CASCADE)
+	accesorio = models.ManyToManyField(accesorio)
